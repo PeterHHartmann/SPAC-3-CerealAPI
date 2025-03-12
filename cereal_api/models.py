4ad77c14211d80ef356778a18987292225cd8a36
PeterHHartmann <peter.helle.hartmann@gmail.com>
2025-03-12 10:41:26 +0100

    cleanup

--- cereal_api/models.py
+++ cereal_api/models.py
@@ -1,7 +1,17 @@
-
 from uuid import uuid4
-from django.db.models import Model, CharField, TextChoices, IntegerChoices, UUIDField, ForeignKey, OneToOneField, CASCADE, PositiveIntegerField, FloatField
+from django.db.models import (
+    Model,
+    CharField,
+    TextChoices,
+    UUIDField,
+    ForeignKey,
+    OneToOneField,
+    CASCADE,
+    PositiveIntegerField,
+    FloatField,
+)
 from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 class Manufacturer(Model):
     class ManufacturerChoices(TextChoices):
@@ -13,48 +23,36 @@ class Manufacturer(Model):
         QUAKER_OATS = "Q"
         RALSTON_PURINA = "R"
 
-    id = UUIDField(
-        primary_key=True, 
-        default=uuid4, 
-        editable=False
-    )
+    id = UUIDField(primary_key=True, default=uuid4, editable=False)
 
-    code = CharField(
-        max_length=1,
-        choices=ManufacturerChoices.choices,
-        null=False
-    )
+    code = CharField(max_length=1, choices=ManufacturerChoices.choices, null=False)
+
 
 class ThermalType(Model):
     class ThermalChoices(TextChoices):
-        COLD = 'C'
-        HOT = 'H'
-        
-    id = UUIDField(
-            primary_key=True, 
-            default=uuid4, 
-            editable=False
-        )
-    code = CharField(
-            max_length=1, 
-            choices=ThermalChoices.choices, 
-            default=ThermalChoices.COLD, 
-            unique=True,
-            null=False
-        )
-    
+        COLD = "C"
+        HOT = "H"
 
-    
+    id = UUIDField(primary_key=True, default=uuid4, editable=False)
+    code = CharField(
+        max_length=1,
+        choices=ThermalChoices.choices,
+        default=ThermalChoices.COLD,
+        unique=True,
+        null=False,
+    )
+
+
 class Cereal(Model):
     name = CharField(max_length=255)
     mfd = ForeignKey(Manufacturer, on_delete=CASCADE)
     type = ForeignKey(ThermalType, on_delete=CASCADE)
-    shelf = PositiveIntegerField(null=False, validators=[
-        MaxValueValidator(3),
-        MinValueValidator(1)
-    ])
+    shelf = PositiveIntegerField(
+        null=False, validators=[MaxValueValidator(3), MinValueValidator(1)]
+    )
     weight = FloatField(null=False)
     cups = FloatField(null=False)
+
 
 class Nutrition(Model):
     cereal = OneToOneField(Cereal, on_delete=CASCADE, primary_key=True)
